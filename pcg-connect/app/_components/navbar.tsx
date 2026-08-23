0c0f52d1d85fd8ff490a36dcd712af4a0a236e83
Jake Nutting” git config --global user.email “nuttingjake@gmail.com”git config --global user.name Jake <nuttingjake@gmail.com>
2026-08-23 14:43:00 -0400

    init commit. Add nextjs, shadcn

---
+++ pcg-connect/app/_components/navbar.tsx
@@ -0,0 +1,9 @@
+export async function Navbar() {
+    return (
+        <nav className="dark:bg-gray-900 shadow-sm">
+            <div className="mx-auto flex max-w-screen-xl flex-wrap items-center justify-between py-4 px-4 xl:px-0">
+                <h4>TESTING</h4>
+            </div>
+        </nav>
+    );
+}
