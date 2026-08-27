ad85663caf2eb8b4bb09e3be99724d0876e8015e
Jake Nutting” git config --global user.email “nuttingjake@gmail.com”git config --global user.name Jake <nuttingjake@gmail.com>
2026-08-26 20:39:56 -0400

    Update

--- pcg-connect/app/_components/navbar.tsx
+++ pcg-connect/app/_components/navbar.tsx
@@ -1,7 +1,13 @@
-import { RadioTower } from "lucide-react";
-import Link from "next/link";
+"use client";
 
-export async function Navbar() {
+import { cn } from "@/lib/utils";
+import { ListCheck } from "lucide-react";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
+
+export function Navbar() {
+    const currentRoute = usePathname();
     return (
         <nav className="dark:bg-gray-900 shadow-sm">
             <div className="mx-auto flex max-w-screen-xl flex-wrap items-center justify-between py-4 px-4 xl:px-0"> 
@@ -10,27 +16,44 @@ export async function Navbar() {
                         href="/"
                         className="self-center flex gap-2 whitespace-nowrap text-xl font-semibold text-gray-600 dark:text-white"
                         >
-                        <RadioTower className="text-cyan-500" />
-                        PCG Connect
+                        <ListCheck className="text-cyan-500" />
+                        GoalDen
                     </Link>
                 </div>
                 <div className="hidden w-full md:block md:w-auto" id="navbar-default">
                     <ul className="mt-4 flex items-center font-medium dark:border-gray-700 dark:bg-gray-800 md:mt-0 md:space-x-8 md:border-0 md:bg-white md:p-0 md:dark:bg-gray-900">
                         <li>
-                        <Link
-                            href="/"
-                            className="block rounded px-3 py-2 dark:text-white md:bg-transparent md:p-0 text-cyan-500"
-                        >
-                            Home
-                        </Link>
+                            <Link
+                                className={cn(
+                                    "block rounded-lg dark:text-white px-3 py-2 text-gray-700 hover:bg-cyan-50",
+                                    currentRoute === "/" && "bg-cyan-50"
+                                )}
+                                href="/"
+                                >
+                                Home
+                            </Link>
                         </li>
                         <li>
-                            <a
-                                href="#"
-                                className="block rounded px-3 py-2 text-gray-700 hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:border-0 md:p-0 md:hover:bg-transparent md:hover:text-blue-700 md:dark:hover:bg-transparent md:dark:hover:text-blue-500"
+                            <Link
+                                href="/events"
+                                 className={cn(
+                                    "block rounded-lg dark:text-white px-3 py-2 text-gray-700 hover:bg-cyan-50 hover:text-cyan-600",
+                                    currentRoute.includes("/events") && "bg-cyan-50 text-cyan-600"
+                                )}
+                            >
+                                Dashboard
+                            </Link>
+                        </li>
+                         <li>
+                            <Link
+                                href="/about"
+                                 className={cn(
+                                    "block rounded-lg dark:text-white px-3 py-2 text-gray-700 hover:bg-cyan-50 hover:text-cyan-600",
+                                    currentRoute.includes("/about") && "bg-cyan-50 text-cyan-600"
+                                )}
                             >
                                 About
-                            </a>
+                            </Link>
                         </li>
                         {/* <li>
                         <OrganizationSwitcher />
--- pcg-connect/app/page.tsx
+++ pcg-connect/app/page.tsx
@@ -1,69 +1,48 @@
-import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert h-5 w-[100px]"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the{" "}
-            <code className="rounded bg-black/[.06] px-1.5 py-0.5 font-mono text-[0.9em] dark:bg-white/[.08]">
-              page.tsx
-            </code>{" "}
-            file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
+    <main>
+      <div className="bg-gray-100 flex justify-between items-center px-24 py-8 gap-2 ">
+        <div>
+          <h3 className="text-3xl font-extrabold mt-6">Goal <span className="text-cyan-500">Den</span></h3>
+          <p className="text-lg text-gray-600 dark:text-gray-400">
+            Welcome to GoalDen, a platform to transform your daily lives
+          </p>
+          <div className="mt-6">
+            <Link
+              href={"/events"}
+              className="bg-cyan-500 hover:bg-cyan-600 transition-colors text-white font-bold py-2 px-4 rounded-lg">
+              Get Started
+            </Link>
+          </div>
+        </div>
+
+        <img src="/collaboration.svg" alt="PCG Connect" className="size-64" />
+      </div>
+
+      <div className="grid grid-cols-3 px-24 mt-24 gap-12">
+        <div className="bg-white rounded-lg shadow-lg p-5">
+          <h3 className="text-lg font-bold mb-2">Plan</h3>
+          <p className="text-gray-600 dark:text-gray-400">
+            Plan your events and activities with ease using our intuitive planning tools.
           </p>
         </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert h-[14px] w-4"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={14}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
+
+        <div className="bg-white rounded-lg shadow-lg p-5">
+          <h3 className="text-lg font-bold mb-2">Connect</h3>
+          <p className="text-gray-600 dark:text-gray-400">
+            Connect and integrate your daily life with the entire family
+          </p>
         </div>
-      </main>
-    </div>
+
+        <div className="bg-white rounded-lg shadow-lg p-5">
+          <h3 className="text-lg font-bold mb-2">Collaborate</h3>
+          <p className="text-gray-600 dark:text-gray-400">
+            Work together on projects and initiatives that make a difference in our community.
+          </p>
+        </div>
+      </div>
+    </main>
   );
 }
