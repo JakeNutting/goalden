05d359bcfa0f7ac70135f5162aeac8b0204628ed
Jake Nutting” git config --global user.email “nuttingjake@gmail.com”git config --global user.name Jake <nuttingjake@gmail.com>
2026-09-06 13:23:46 -0400

    Added bottom navbar

--- goalden/app/_components/navbar.tsx
+++ goalden/app/_components/navbar.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { cn } from "@/lib/utils";
-import { ListCheck } from "lucide-react";
+import { Home, ListCheck, ListChecks, ShelvingUnit } from "lucide-react";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 
@@ -20,6 +20,42 @@ export function Navbar() {
                         GoalDen
                     </Link>
                 </div>
+                {/* Mobile UI */}
+                <nav className="fixed inset-x-0 bottom-0 z-9999 w-full border-t-2 border-gray-100 bg-white md:hidden dark:bg-gray-900">
+                    <div className="flex items-center divide-x-2 text-xs">
+                        <Link
+                            className={cn(
+                                "flex-1 dark:text-white text-gray-700 hover:bg-cyan-50 p-2 flex flex-col items-center gap-1.5",
+                                currentRoute === "/" && "bg-cyan-50"
+                            )}
+                            href="/"
+                            >
+                            <Home></Home>
+                            Home
+                        </Link>
+                        <Link
+                            className={cn(
+                                "flex-1 dark:text-white text-gray-700 hover:bg-cyan-50 p-2 flex flex-col items-center gap-1.5",
+                                currentRoute === "/inventory" && "bg-cyan-50 text-cyan-600 font-semibold"
+                            )}
+                            href="/inventory"
+                            >
+                            <ShelvingUnit></ShelvingUnit>
+                            Inventory
+                        </Link>
+                        <Link
+                            className={cn(
+                                "flex-1 dark:text-white text-gray-700 hover:bg-cyan-50 p-2 flex flex-col items-center gap-1.5",
+                                currentRoute === "/grocery-list" && "bg-cyan-50"
+                            )}
+                            href="/grocery-list"
+                            >
+                            <ListChecks></ListChecks>
+                            Grocery List
+                        </Link>
+
+                    </div>
+                </nav>
                 <div className="hidden w-full md:block md:w-auto" id="navbar-default">
                     <ul className="mt-4 flex items-center font-medium dark:border-gray-700 dark:bg-gray-800 md:mt-0 md:space-x-8 md:border-0 md:bg-white md:p-0 md:dark:bg-gray-900">
                         <li>
@@ -35,13 +71,23 @@ export function Navbar() {
                         </li>
                         <li>
                             <Link
-                                href="/events"
+                                href="/dashboard"
                                  className={cn(
                                     "block rounded-lg dark:text-white px-3 py-2 text-gray-700 hover:bg-cyan-50 hover:text-cyan-600",
                                     currentRoute.includes("/dashboard") && "bg-cyan-50 text-cyan-600"
                                 )}
                             >
                                 Dashboard
+                            </Link>
+                        </li>
+                        <li>
+                            <Link
+                                href="/inventory"
+                                 className={cn(
+                                    "block rounded-lg dark:text-white px-3 py-2 text-gray-700 hover:bg-cyan-50 hover:text-cyan-600",
+                                    currentRoute.includes("/inventory") && "bg-cyan-50 text-cyan-600"
+                                )}>
+                                Inventory
                             </Link>
                         </li>
                          <li>
--- goalden/app/dashboard/page.tsx
+++ goalden/app/dashboard/page.tsx
@@ -1,5 +1,7 @@
 export default function Events() {
-    return <>
-        &quot;AOSDMAOSMD&quot;
-    </>
+    return (
+        <div className="bg-gray-100 flex justify-between items-center px-24 py-8 gap-2 ">
+            <h4 className="font-medium text-sm">Manage Inventory</h4>
+        </div>
+    )
 }
